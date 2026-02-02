5d623a5b162216f55880042b2255c9c4854bfc5b
hwangfucius <junzhe.hwangfu@gmail.com>
2026-02-02 20:14:46 +0100

    feat: add IssueType

--- crates/core/src/outcome.rs
+++ crates/core/src/outcome.rs
@@ -9,3 +9,38 @@ pub enum IssueSeverity {
     Warning,
     Information,
 }
+
+/// Type of issue
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[serde(rename_all = "kebab-case")]
+pub enum IssueType {
+    Invalid,
+    Structure,
+    Required,
+    Value,
+    Invariant,
+    Security,
+    Login,
+    Unknown,
+    Expired,
+    Forbidden,
+    Suppressed,
+    Processing,
+    NotSupported,
+    Duplicate,
+    NotFound,
+    TooLong,
+    CodeInvalid,
+    Extension,
+    TooCostly,
+    BusinessRule,
+    Conflict,
+    Incomplete,
+    Transient,
+    LockError,
+    NoStore,
+    Exception,
+    Timeout,
+    Throttled,
+    Informational,
+}
