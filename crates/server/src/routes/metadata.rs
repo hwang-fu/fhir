09f856e512e617f7d6115542d401df7343068149
hwangfucius <junzhe.hwangfu@gmail.com>
2026-02-02 22:06:52 +0100

    feat: add get()

---
+++ crates/server/src/routes/metadata.rs
@@ -0,0 +1,8 @@
+
+use axum::Json;
+use fhir_core::CapabilityStatement;
+
+/// GET /metadata - Return server capability statement
+pub async fn get() -> Json<CapabilityStatement> {
+    Json(CapabilityStatement::new())
+}
